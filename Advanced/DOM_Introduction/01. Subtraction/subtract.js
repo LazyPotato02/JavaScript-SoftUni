be0c0909b3a7c2333184cf455f57c60326c8d0be
LazyPotato02 <74728512+LazyPotato02@users.noreply.github.com.>
2024-02-02 23:37:41 +0200

    Added 2 solutions

--- Advanced/DOM_Introduction/01. Subtraction/subtract.js
+++ Advanced/DOM_Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,9 @@
 function subtract() {
-    console.log('TODO:...');
+
+    let firstNumber = document.getElementById('firstNumber').value
+    let secondNumber = document.getElementById('secondNumber').value
+    let resultDiv = document.getElementById('result')
+    let result = Number(firstNumber) - Number(secondNumber)
+    resultDiv.textContent = result
+
 }
--- Advanced/DOM_Introduction/02. Pascal or Camel Case/solution.js
+++ Advanced/DOM_Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,42 @@
 function solve() {
-  //TODO...
+    let resultContainer = document.getElementById('result')
+    let text = document.getElementById('text').value
+    let namingConvention = document.getElementById('naming-convention').value
+
+    if (namingConvention === 'Camel Case') {
+        let textList = text.split(' ');
+        let convertedArr = []
+        let count = 0
+        for (let element of textList) {
+            element = element.toLowerCase()
+            if (count > 0) {
+                let firstChar = element[0].toUpperCase()
+                let slicedElement = element.slice(1)
+                let result = firstChar + slicedElement
+                convertedArr.push(result)
+                continue
+            } else {
+                convertedArr.push(element)
+            }
+            count++
+        }
+        resultContainer.textContent = convertedArr.join('')
+
+    } else if (namingConvention === 'Pascal Case') {
+        let textList = text.split(' ');
+        let convertedArr = []
+        for (let element of textList) {
+            element = element.toLowerCase()
+            let firstChar = element[0].toUpperCase()
+            let slicedElement = element.slice(1)
+            let result = firstChar + slicedElement
+            convertedArr.push(result)
+        }
+        resultContainer.textContent = convertedArr.join('')
+
+    } else {
+        resultContainer.textContent = 'Error!'
+
+    }
 }
+
