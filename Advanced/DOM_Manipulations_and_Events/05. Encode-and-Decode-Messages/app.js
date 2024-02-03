88ab378ff753c09325a5649c6bba0623ce82fb6f
LazyPotato02 <74728512+LazyPotato02@users.noreply.github.com.>
2024-02-03 17:58:38 +0200

    Added solutions

--- Advanced/DOM_Manipulations_and_Events/05. Encode-and-Decode-Messages/app.js
+++ Advanced/DOM_Manipulations_and_Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,39 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeSendBtn = document.getElementsByTagName('button')[0]
+    let decodeReceiveBtn = document.getElementsByTagName('button')[1]
+
+    let encodeTextArea = document.getElementsByTagName('textarea')[0]
+    let decodeTextArea = document.getElementsByTagName('textarea')[1]
+
+    encodeSendBtn.addEventListener("click", encode)
+    decodeReceiveBtn.addEventListener("click", decode)
+
+
+
+    function encode() {
+        let resultArray = []
+        let string = encodeTextArea.value
+        for (let i = 0; i < string.length; i++) {
+            let code = string.charCodeAt(i) + 1
+            while (code > 122) {
+                code = (code - 122) + 96
+            }
+            resultArray.push(String.fromCharCode(code))
+        }
+        encodeTextArea.value = ''
+        decodeTextArea.value = resultArray.join('')
+    }
+
+    function decode() {
+        let resultArray = []
+        let string = decodeTextArea.value
+        for (let i = 0; i < string.length; i++) {
+            let code = string.charCodeAt(i) - 1
+            while (code > 122) {
+                code = (code - 122) + 96
+            }
+            resultArray.push(String.fromCharCode(code))
+        }
+        decodeTextArea.value = resultArray.join('')
+    }
 }
