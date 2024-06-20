140857d891706727b2ff182672f86c88513f6ced
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-06-20 22:27:43 +0300

    Start of exam prep #2

---
+++ JS_Back_end/exam-prep-2/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {configDatabase} = require("./config/configDatabase");
+const {configExpress} = require("./config/configExpress");
+const {configHandlebars} = require("./config/configHandlebars");
+const {configRoutes} = require("./config/configRoutes");
+
+
+start()
+async function start(){
+    const app = express();
+
+    await configDatabase()
+    configHandlebars(app)
+    configExpress(app)
+    configRoutes(app)
+
+    app.listen(3000, () => {
+        console.log('Server started on port 3000  http://localhost:3000')
+
+
+    });
+}
+
+
